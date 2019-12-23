1cc8a330ef535cc024c31a982b9cc7349533c445
Rico Castelo <ricorx7@gmail.com>
2019-12-23 00:57:28 -0800

    Adding Themeing Working on river processing

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -4,6 +4,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import MiniDrawer from './menu'
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import pink from "@material-ui/core/colors/pink";
+import blue from "@material-ui/core/colors/blue";
 
 /**
  * Parameters for the display.
@@ -29,12 +33,26 @@ class App extends React.Component<AppProps, AppState> {
 
   }
 
+  theme = createMuiTheme({
+    palette: {
+      primary: blue,
+      secondary: {
+        light: "#ff79b0",
+        main: pink.A200,
+        dark: "#c60055",
+        contrastText: "#fff"
+      }
+    }
+  });
 
   render() {
     return (
-      <div>
+      
+      <MuiThemeProvider theme={this.theme}>
+        <CssBaseline />
         <MiniDrawer />
-      </div>
+      </MuiThemeProvider>
+
 
     );
   }
